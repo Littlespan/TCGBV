e5a15bbfd34f45d3cb7f8abf2e0049970333bf6d
Administrator <15205698133>
2020-11-02 15:54:01 +0800

    2020/11/02/pm

--- sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import cn.tedu.web.util.JsonUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
  */
 @Slf4j
 @Service
+@RefreshScope
 public class UserServiceImpl implements UserService {
     @Value("${sp.user-service.users}")
     private String userJson;
